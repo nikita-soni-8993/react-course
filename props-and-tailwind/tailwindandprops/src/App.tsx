e49c3c30e8b4770fe6f0cbc21353c45c52910c2c
nikita-soni-8993 <61220665+nikita-soni-8993@users.noreply.github.com>
2025-04-08 19:25:43 +0000

    done

---
+++ props-and-tailwind/tailwindandprops/src/App.tsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.css'
+import Card from'./components/Card';
+
+function App() {
+ // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-400 rounded-xl text-black p-4' mb-4>Hi Tailwind</h1>
+      <Card/>
+        
+    </>
+  )
+}
+
+export default App
